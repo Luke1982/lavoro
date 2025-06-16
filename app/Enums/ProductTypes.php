51321b5edf641d34d8c60997f9935b0b1925a8ed
Luke1982 <info@majorlabel.nl>
2025-06-16 16:06:42 +0200

    model(Product) Better translation

--- app/Enums/ProductTypes.php
+++ app/Enums/ProductTypes.php
@@ -6,7 +6,7 @@ enum ProductTypes: string
 {
     case lift          = 'Hefbrug';
     case tirechanger   = 'Bandenwisselaar';
-    case wheelbalancer = 'Balansmachine';
+    case wheelbalancer = 'Balanceermachine';
     case compressor    = 'Compressor';
     case alignment     = 'Uitlijnapparatuur';
     case other         = 'Overig';
