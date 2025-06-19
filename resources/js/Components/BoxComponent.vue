7a919d16b479a8e49fe30641434dc0de429eef55
Luke1982 <info@majorlabel.nl>
2025-06-19 20:06:07 +0200

    feat(Components) Add Box

---
+++ resources/js/Components/BoxComponent.vue
@@ -0,0 +1,6 @@
+<template>
+    <div
+        class="bg-white -mx-4 px-4 py-8 shadow-xs ring-1 ring-gray-900/5 sm:mx-0 sm:rounded-lg sm:px-8 sm:pb-14 lg:col-span-2 lg:row-span-2 lg:row-end-2 xl:px-8 xl:pt-8 xl:pb-8">
+        <slot></slot>
+    </div>
+</template>
