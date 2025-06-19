1552a533d518a6884e107dacca55905808cfa6c6
Luke1982 <info@majorlabel.nl>
2025-06-19 20:06:49 +0200

    feat(Layouts) Add two thirds / one third layout

---
+++ resources/js/Layouts/TwoThirdsOneThird.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex flex-wrap md:flex-nowrap">
+        <div class="w-100 lg:w-2/3 mt-5 lg:mt-0">
+            <slot name="main"></slot>
+        </div>
+        <div class="w-100 lg:w-1/3 lg:ml-10 mt-5 lg:mt-0">
+            <slot name="sidebar"></slot>
+        </div>
+    </div>
+</template>
