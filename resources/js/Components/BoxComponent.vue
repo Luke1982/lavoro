52d9d91dbf2b78cd4829248b29d386bde279a4f6
Luke1982 <info@majorlabel.nl>
2025-07-01 16:17:16 +0200

    feat(Box) Allow setting custom padding on component

--- resources/js/Components/BoxComponent.vue
+++ resources/js/Components/BoxComponent.vue
@@ -1,6 +1,15 @@
 <template>
     <div
-        class="bg-white -mx-4 px-4 py-8 shadow-xs ring-1 ring-gray-900/5 sm:mx-0 sm:rounded-lg sm:px-8 sm:pb-14 lg:col-span-2 lg:row-span-2 lg:row-end-2 xl:px-8 xl:pt-8 xl:pb-8">
+        :class="[padding, 'bg-white -mx-4 shadow-xs ring-1 ring-gray-900/5 sm:mx-0 sm:rounded-lg lg:col-span-2 lg:row-span-2 lg:row-end-2']">
         <slot></slot>
     </div>
 </template>
+
+<script setup>
+defineProps({
+    padding: {
+        type: String,
+        default: 'px-4 py-8 xl:px-8 xl:pt-8 xl:pb-8 sm:px-8 sm:pb-14 px-4 py-8',
+    },
+});
+</script>
