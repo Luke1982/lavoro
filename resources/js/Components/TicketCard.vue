bc43b8d6096e620d70757fcd43666e2f45aac8e1
Luke1982 <info@majorlabel.nl>
2025-07-04 15:00:00 +0200

    feat(Ticket) Add ticketcard component

---
+++ resources/js/Components/TicketCard.vue
@@ -0,0 +1,65 @@
+<template>
+    <Link :href="`/tickets/${ticket.id}`" :class="[getWrapperClasses(ticket.status), 'rounded-md p-2 border-2 block']">
+    <div class="flex">
+        <div class="flex flex-col items-center justify-start mr-2 w-20">
+            <ExclamationCircleIcon :class="[getExclamationMarkClasses(ticket.priority, ticket.status), 'w-6 h-6']" />
+            <span :class="[getExclamationMarkClasses(ticket.priority, ticket.status), 'text-xs']">Prio {{
+                ticket.priority }}</span>
+        </div>
+        <div class="flex flex-col">
+            <h1 class="text-l font-bold">{{ ticket.subject }}</h1>
+            <div class="flex mt-1">
+                <CalendarDateRangeIcon class="w-5 h-5 text-gray-500 mr-1" />
+                <div class="text-sm text-gray-500">
+                    <span>Sinds {{ nlDate(ticket.created_at) }}</span>
+                    <span v-if="ticket.closed_on && ticket.status.toLowerCase() === 'gesloten'">, Gesloten op {{
+                        nlDate(ticket.closed_on) }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    </Link>
+</template>
+
+<script setup>
+import { CalendarDateRangeIcon, ExclamationCircleIcon } from '@heroicons/vue/24/outline';
+import { Link } from '@inertiajs/vue3';
+import { nlDate } from '@/Utilities/Utilities';
+
+defineProps({
+    ticket: {
+        type: Object,
+        required: true,
+    },
+});
+
+const getExclamationMarkClasses = (priority, status) => {
+    const lowerPriority = priority.toLowerCase();
+    const lowerStatus = status.toLowerCase();
+    if (lowerStatus === 'gesloten') {
+        return 'text-gray-500';
+    }
+    if (lowerPriority === 'hoog') {
+        return 'text-red-700';
+    } else if (lowerPriority === 'laag') {
+        return 'text-green-700';
+    } else if (lowerPriority === 'normaal') {
+        return 'text-yellow-700';
+    } else {
+        return 'text-gray-700';
+    }
+}
+
+const getWrapperClasses = status => {
+    const lowerStatus = status.toLowerCase();
+    if (lowerStatus === 'open') {
+        return 'bg-red-100 border-red-200 text-red-700';
+    } else if (lowerStatus === 'gesloten') {
+        return 'bg-green-100 border-green-200 text-green-700';
+    } else if (lowerStatus === 'in behandeling') {
+        return 'bg-yellow-100 border-yellow-200 text-yellow-700';
+    } else {
+        return 'bg-gray-100 border-gray-200 text-gray-700';
+    }
+}
+</script>
