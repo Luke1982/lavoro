e43fa3fe380a5b6790818bb4237d7f918f2534a8
Luke1982 <info@majorlabel.nl>
2025-08-11 12:59:21 +0200

    feat(UI) Add colorpicker component

---
+++ resources/js/Components/UI/ColorPickerComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <ColorPicker alpha-channel="hide" :color="modelValue" default-format="rgb" @color-change="onColorChange">
+        <template #hue-range-input-label>Basiskleur</template>
+    </ColorPicker>
+</template>
+<script setup lang="ts">
+import { ColorPicker } from 'vue-accessible-color-picker';
+
+const emit = defineEmits(['update:modelValue']);
+
+const { modelValue } = defineProps<{
+    modelValue: string;
+}>();
+
+function onColorChange(event: { colors: { hex } }) {
+    console.log('Color changed:', event.colors.hex);
+    emit('update:modelValue', event.colors.hex);
+}
+</script>
+<style>
+@import url('vue-accessible-color-picker/styles');
+
+.vacp-actions,
+.vacp-format-switch-button,
+.vacp-color-input-group {
+    display: none !important;
+}
+</style>
