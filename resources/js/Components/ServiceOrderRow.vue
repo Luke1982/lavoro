61d48fe0aeb8ec9aac104aa45744743185276b4d
Luke1982 <info@majorlabel.nl>
2025-08-21 13:42:54 +0200

    resp(ServiceOrders) flex-wrap the data in the row component

--- resources/js/Components/ServiceOrderRow.vue
+++ resources/js/Components/ServiceOrderRow.vue
@@ -1,26 +1,26 @@
 <template>
     <div class="relative odd:bg-gray-50 even:bg-white border-gray-100 border-1 flex flex-col p-2">
         <Link :href="`/serviceorders/${serviceorder.id}`">
-        <div class="flex py-0.5">
+        <div class="flex py-0.5 flex-wrap">
             <span class="font-semibold text-xs min-w-25 max-w-25">Gemaakt op:</span>
             <span class="text-sm">{{
                 nlDate(serviceorder.created_at)
                 }}</span>
         </div>
-        <div class="flex py-0.5">
+        <div class="flex py-0.5 flex-wrap">
             <span class="font-semibold text-xs min-w-25 max-w-25">Gesloten:</span>
             <span class="text-sm" v-if="serviceorder.closed_on">{{
                 nlDate(serviceorder.closed_on)
                 }}</span>
             <span class="text-xs p-0.5 px-2 ring-red-200 ring-2 rounded-full my-1 bg-red-50" v-else>Nee</span>
         </div>
-        <div class="flex py-0.5" v-if="serviceorder.closed_on">
+        <div class="flex py-0.5 flex-wrap" v-if="serviceorder.closed_on">
             <span class="font-semibold text-xs min-w-25 max-w-25">Getekend door:</span>
             <span class="text-sm">{{
                 serviceorder.signed_by
                 }}</span>
         </div>
-        <div class="flex py-0.5">
+        <div class="flex py-0.5 flex-wrap">
             <span class="font-semibold text-xs min-w-25 max-w-25">Keuringen:</span>
             <span class="text-sm">{{
                 serviceorder.service_jobs.length
