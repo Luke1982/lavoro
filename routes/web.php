810e73490a5a488036c3e919dec06b153be9cd7b
Luke1982 <info@majorlabel.nl>
2025-08-29 16:31:59 +0200

    fix(Auth) Allow logging out through GET

--- routes/web.php
+++ routes/web.php
@@ -77,4 +77,4 @@ Route::group(
 
 Route::get('login', [AuthController::class, 'create'])->name('login');
 Route::post('login', [AuthController::class, 'store'])->name('login.store');
-Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
+Route::get('logout', [AuthController::class, 'destroy'])->name('logout');
