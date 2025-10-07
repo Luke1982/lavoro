68a296f128607d3aa26696756da641db10ce9d81
Luke1982 <info@majorlabel.nl>
2025-10-07 10:11:29 +0200

    feat(Company) Responsiveness

--- resources/js/Pages/Companies/Partials/EditCompanyModal.vue
+++ resources/js/Pages/Companies/Partials/EditCompanyModal.vue
@@ -2,7 +2,7 @@
     <Dialog :open="true" as="div" class="relative z-10" @close="$emit('close')">
         <div class="fixed inset-0 bg-gray-500/75 dark:bg-slate-900/75 transition-opacity" />
         <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div class="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
                 <div
                     class="relative transform overflow-hidden rounded-lg bg-white dark:bg-slate-900 px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6 border dark:border-slate-800">
                     <div class="sm:flex sm:items-start">
