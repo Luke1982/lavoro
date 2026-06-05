9ab0ba471165785a2060c0208ea834c186703bd3
Luke1982 <info@majorlabel.nl>
2026-06-05 21:21:47 +0200

    feat(products): add ProductBulkUpdateAttributesRequest

---
+++ app/Http/Requests/ProductBulkUpdateAttributesRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\ProductAttributeValue;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductBulkUpdateAttributesRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user()->hasPermission('product.update');
+    }
+
+    public function rules(): array
+    {
+        return [
+            'product_ids'                             => ['required', 'array', 'min:1'],
+            'product_ids.*'                           => ['integer', 'exists:products,id'],
+            'attributes'                              => ['required', 'array', 'min:1'],
+            'attributes.*.product_attribute_id'       => ['required', 'integer', 'exists:product_attributes,id'],
+            'attributes.*.product_attribute_value_id' => [
+                'required',
+                'integer',
+                'exists:product_attribute_values,id',
+                function (string $attribute, mixed $value, \Closure $fail) {
+                    $index  = explode('.', $attribute)[1];
+                    $attrId = $this->input("attributes.{$index}.product_attribute_id");
+                    if (! ProductAttributeValue::where('id', $value)
+                        ->where('product_attribute_id', $attrId)
+                        ->exists()) {
+                        $fail('De geselecteerde waarde hoort niet bij het opgegeven kenmerk.');
+                    }
+                },
+            ],
+        ];
+    }
+}
