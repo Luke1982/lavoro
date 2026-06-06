5f71bf226d4ccf5d93028717608c0c8e5a4ea7f6
Luke1982 <info@majorlabel.nl>
2026-06-06 19:57:04 +0200

    feat(events): add create_service_order + customer_id validation rules

--- app/Http/Requests/EventStoreRequest.php
+++ app/Http/Requests/EventStoreRequest.php
@@ -41,9 +41,12 @@ class EventStoreRequest extends FormRequest
             'status' => 'required|in:Gepland,Gaande,Afgerond,Geannuleerd',
             'start' => 'required|date_format:Y-m-d H:i',
             'end' => 'required|date_format:Y-m-d H:i|after_or_equal:start',
-            'eventable_type' => 'nullable|string|in:\\App\\Models\\ServiceOrder',
-            'eventable_id' => 'nullable|exists:service_orders,id',
-            'executing_user_ids' => 'required|array|min:1',
+            'location' => 'nullable|string|max:255',
+            'eventable_type'       => 'nullable|string|in:\\App\\Models\\ServiceOrder',
+            'eventable_id'         => 'nullable|exists:service_orders,id',
+            'create_service_order' => 'nullable|boolean',
+            'customer_id'          => 'required_if:create_service_order,true|nullable|exists:customers,id',
+            'executing_user_ids'   => 'required|array|min:1',
             'executing_user_ids.*' => 'exists:users,id',
         ];
     }
